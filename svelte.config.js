d778b262d538973e0eee306b763f79b9dafce6af
George Prichard <dgmprichard@gmail.com>
2023-03-06 21:53:20 +0000

    revert, this borked things

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -21,10 +19,7 @@ const config = {
 			fallback: null,
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			base: dev ? '' : '/CheapGPT'
-		}
+		})
 	}
 };
 
