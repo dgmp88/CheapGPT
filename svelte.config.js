73bfa73ad670f66dd7bdaa29003c8c0783498bc0
George Prichard <dgmprichard@gmail.com>
2023-03-06 21:51:36 +0000

    Github pages fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
-
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -20,7 +21,10 @@ const config = {
 			fallback: null,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/CheapGPT'
+		}
 	}
 };
 
