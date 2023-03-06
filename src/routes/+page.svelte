1b14b1d27c9b6f69e4c433c0d30adc006bd06ee2
George Prichard <dgmprichard@gmail.com>
2023-03-06 22:00:11 +0000

    Typography defaults

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,23 +28,23 @@
 </svelte:head>
 
 <section>
-	<div class="flex flex-col h-screen static">
-		<h2 class="text-center text-2xl font-semibold">CheapGPT</h2>
+	<div class="flex flex-col h-screen static p-4">
+		<h1 class="text-center text-6xl font-semibold pb-4">CheapGPT</h1>
 		{#if requestApiKey}
 			<article class="prose">
-				<h1>API Key is not set</h1>
+				<h2>API Key is not set</h2>
 				<p>
 					Get an API key from <a href="https://platform.openai.com/account/api-keys">here</a> and paste
 					it below.
 				</p>
 				<p>
-					Is it secure? Not particularly :D. I'm not sending it anywhere except to OpenAPI though,
-					see here INSERT GITHUB LINK
-				</p>
-				<p>
 					I strongly recommend setting a <a
 						href="https://platform.openai.com/account/billing/limits">usage limit</a
 					> you can afford to lose
+				</p>
+				<p>
+					Is it secure? Not particularly :D. I'm not sending it anywhere except to OpenAPI though,
+					see <a href="https://github.com/dgmp88/CheapGPT">here</a> for the full source code.
 				</p>
 			</article>
 			<TextInput onComplete={onSubmitApiKey} />
